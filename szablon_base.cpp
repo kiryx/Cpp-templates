ec848bef6523fd075cbf5cf5a59d9c07e6fa7f44
Krzysiek <kiryx7@gmail.com>
2011-04-09 21:31:56 +0200

    graf i podstawka

---
+++ szablon_base.cpp
@@ -0,0 +1,34 @@
+
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector< VI > VVI;
+typedef long long LL;
+typedef int INF = 1000000001;
+typedef pair<int, int> PII;
+typedef vector< PII > VPII;
+typedef vector< VPII > VVPII;
+
+#define FOR(x,b,e) for(int x=b; x<=(e);++x)
+#define FORD(x,b,e) for(intx=b; x>=(e);--x)
+#define REP(x,n) for(int x=0;x<(n);++x)
+#define VAR(v,n) __typeof(n) v=(n)
+#define ALL(c) c.begin(),c.end()
+#define SIZE(x) (int)x.size()
+#define FOREACH(i,c) for(VAR(i,(c).begin());i!=(c).end();++i)
+#define PB push_back
+#define ST first
+#define ND second
+
+
+int main() {
+ 
+  
+	return 0;
+}
---
+++ szablon_graf.cpp
@@ -0,0 +1,29 @@
+template <class V, class E> struct Graph {
+	struct Ed : E {
+		int v;
+		Ed(E p, int w) : E(p), v(w) { }
+	};
+	struct Ve : V, vector<Ed> { };
+	vector<Ve> g;
+	Graph(int n = 0) : g(n) { }
+	void Write() {
+		REP(i,SIZE(g)) {
+			cout<< i << ":";
+			FOREACH(it,g[i])
+				cout<< " " << it->v;
+			cout<< endl;
+		}
+	}
+	void EdgeU(int b, int e, E d = E()) {
+		Ed eg(d,e);
+		eg.rev = SIZE(g[e]) + (b==e);
+		g[b].PB(eg);
+		eg.rev = SIZE(g[eg.v = b]) - 1;
+		g[e].PB(eg);
+	}
+	void EdgeD(int b, int e, E d = E()) {
+		g[b].PB(Ed(d,e));
+	}
+};
+struct Empty { };
+struct MyVert { int rev; };
